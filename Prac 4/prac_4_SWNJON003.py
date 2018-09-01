a245c2fe24c6f58b2fe7967290d80858fad1d8d0
Jonah Swain <jonahswain@gmail.com>
2018-09-01 20:20:33 +0200

    Initial commit - added folder/file

---
+++ Prac 4/prac_4_SWNJON003.py
@@ -0,0 +1,23 @@
+# =======================
+# EEE3096S Prac 4
+# 01/09/2018
+# Jonah Swain [SWNJON003]
+# 
+# =======================
+
+# IMPORTS
+import time
+import RPi.GPIO as GPIO
+
+# CLASSES
+
+
+# FUNCTIONS
+
+
+# MAIN FUNCTION
+def main():
+    pass
+
+if (__name__ == "__main__"): # Run main if script is being executed as main
+    main()
