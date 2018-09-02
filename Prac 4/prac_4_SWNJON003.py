d75080984804a0bfd20c9adac9c7715e7daba1f2
Jonah Swain <jonahswain@gmail.com>
2018-09-02 14:43:50 +0200

    added framework classes

--- Prac 4/prac_4_SWNJON003.py
+++ Prac 4/prac_4_SWNJON003.py
@@ -7,9 +7,58 @@
 
 # IMPORTS
 import time
+import datetime
 import RPi.GPIO as GPIO
+import Adafruit_MCP3008
 
 # CLASSES
+class Timer():
+    """A basic timer - tracks how much time has elapsed"""
+
+    def __init__(self):
+        pass
+
+    def get_time(self):
+        pass
+
+class AnalogDevice():
+    """A generic analog device connected to an MCP3008 channel"""
+
+    def __init__(self, mcp3008, channel, differential=False):
+        self.adc = mcp3008
+        self.adcch = channel
+        self.diff = differential
+
+    def get_value(self):
+        pass
+
+class Potentiometer(AnalogDevice):
+    """A potentiometer"""
+
+    def __init__(self, mcp3008, channel):
+        super().__init__(mcp3008, channel)
+
+    def get_value(self):
+        pass
+
+class TempSensor(AnalogDevice):
+    """A potentiometer"""
+
+    def __init__(self, mcp3008, channel):
+        super().__init__(mcp3008, channel)
+
+    def get_value(self):
+        pass
+
+
+class LightSensor(AnalogDevice):
+    """A potentiometer"""
+
+    def __init__(self, mcp3008, channel):
+        super().__init__(mcp3008, channel)
+
+    def get_value(self):
+        pass
 
 
 # FUNCTIONS
