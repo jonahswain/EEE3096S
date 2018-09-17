70535bd05c764eddecedabfe51d40be97c764d18
Jonah Swain <jonahswain@gmail.com>
2018-09-17 13:45:35 +0200

    Added analog device classes and a timer class

--- Prac 4/prac_4_SWNJON003.py
+++ Prac 4/prac_4_SWNJON003.py
@@ -10,16 +10,20 @@ import time
 import datetime
 import RPi.GPIO as GPIO
 import Adafruit_MCP3008
+import threading
 
 # CLASSES
 class Timer():
     """A basic timer - tracks how much time has elapsed"""
 
     def __init__(self):
-        pass
+        self.start_time = time.time()
 
     def get_time(self):
-        pass
+        return time.strftime("%H:%M:%S", time.gmtime(time.time() - self.start_time))
+
+    def reset(self):
+        self.start_time = time.time()
 
 class AnalogDevice():
     """A generic analog device connected to an MCP3008 channel"""
@@ -30,7 +34,10 @@ class AnalogDevice():
         self.diff = differential
 
     def get_value(self):
-        pass
+        if (self.diff):
+            return self.adc.read_adc_difference(self.adcch)
+        else:
+            return self.adc.read_adc(self.adcch)
 
 class Potentiometer(AnalogDevice):
     """A potentiometer"""
@@ -39,26 +46,52 @@ class Potentiometer(AnalogDevice):
         super().__init__(mcp3008, channel)
 
     def get_value(self):
-        pass
+        return (str("{0:.2f}".format(3.3*(super().get_value()/1023))) + " V")
 
 class TempSensor(AnalogDevice):
-    """A potentiometer"""
+    """A temperature sensor"""
 
     def __init__(self, mcp3008, channel):
         super().__init__(mcp3008, channel)
 
     def get_value(self):
-        pass
+        return (str(int((super().get_value()-155)/3.1)) + " °C")
 
 
 class LightSensor(AnalogDevice):
-    """A potentiometer"""
+    """An LDR based light sensor"""
+
+    low_calibration = 0
+    high_calibration = 1023
 
     def __init__(self, mcp3008, channel):
         super().__init__(mcp3008, channel)
 
     def get_value(self):
+        light_value = ((super().get_value() - LightSensor.low_calibration)/(LightSensor.high_calibration - LightSensor.low_calibration))*100
+        return (str(int(light_value)) + " %")
+
+
+
+class Prac4(threading.Thread):
+    """The main practical class"""
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def reset_sw(self): # Reset button pressed
         pass
+
+    def frequency_sw(self): # Frequency button pressed
+        pass
+
+    def stop_sw(self): # Stop button pressed
+        pass
+
+    def display_sw(self): # Display button pressed
+        pass
+
+
 
 
 # FUNCTIONS
